4920a8bb8d7b715476dd1a818e7b70a9edfd8649
Aakash Dinkar <aakashdinkar1307@gmail.com>
2020-09-03 21:42:13 +0530

    Update changes

--- WordDic/views.py
+++ WordDic/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import requests
 import json
+import random
 
 
 def index(request):
@@ -12,61 +13,92 @@ def add_to_dic(dic, key, value):
     else:
         dic[key] = [value]
 
-# word = data["id"]
+def get_data(word):
+    app_id = "ff4a096a"
+    app_key = "0ec8192d96bc98bff0b70f92368f8bdd"
+    language = "en-gb"
+    url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word.lower()
+    r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
+    data = r.json()
+
+    result = {}
+    for item in range(len(data["results"])):
+        for key, value in data["results"][item].items():
+            add_to_dic(result, key, value)
+
+    lexicalEntries = {}
+    for item in range(len(result["lexicalEntries"][0])):
+        for key, value in result["lexicalEntries"][0][item].items():
+            add_to_dic(lexicalEntries, key, value)
+    entries = {}
+    for item in range(len(lexicalEntries["entries"])):
+        for item2 in range(len(lexicalEntries["entries"][item])):
+            for key, value in lexicalEntries["entries"][item][item2].items():
+                add_to_dic(entries, key, value)
+    defi = {}
+    for key in entries:
+        for i in range(len(entries[key])):
+            for j in range(len(entries[key][i])):
+                if isinstance(entries[key][i][j], str):
+                    print(entries[key][i][j])
+                else:
+                    for k, v in entries[key][i][j].items():
+                        add_to_dic(defi, k, v)
+    subsense = {}
+    if "subsenses" in defi:
+        for item in range(len(defi["subsenses"])):
+            for item2 in range(len(defi["subsenses"][item])):
+                for k, v in defi["subsenses"][item][item2].items():
+                    add_to_dic(subsense, k, v)
+    defi["subsenses"] = subsense
+    return defi
+
+def get_images(word):
+    url = 'https://pixabay.com/api/?key=18135885-6452f9fc49c5982740904652b'
+    params = {"per_page":5, "category":"computer", "safesearch":True}
+    params["q"] = word
+    response = requests.get(url, params = params)
+    image = response.json()
+    images = {}
+    for i in range(len(image["hits"])):
+        images[i] = image["hits"][i]["webformatURL"]
+    return images
+
+def get_misc(word):
+    misc = {}
+    url = 'https://api.datamuse.com/words'
+    params = {"rel_ant":f"{word}"}
+    anto = requests.get(url, params = params)
+    ant = anto.json()
+    param = {"rel_jjb":f"{word}"}
+    adje = requests.get(url, params = param)
+    adj = adje.json()
+    if len(adj) >= 5:
+        ran_adj = random.sample(adj, 5)
+        misc["adjective"] = ran_adj
+    else:
+        misc["adjective"] = adj
+
+    par = {"rel_jja":f"{word}"}
+    n = requests.get(url, params = par)
+    noun = n.json()
+    if len(noun) >= 5:
+        ran_noun = random.sample(noun, 5)
+        misc["noun"] = ran_noun
+    else:
+        misc["noun"] = noun
+    misc["antonyms"] = ant
+
+    return misc
 
 def search(request):
     if request.method == 'POST':
         word = request.POST.get("word")
-        app_id = "ff4a096a"
-        app_key = "0ec8192d96bc98bff0b70f92368f8bdd"
-        language = "en-gb"
-        url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word.lower()
-        r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
-        data = r.json()
-
-        result = {}
-        for item in range(len(data["results"])):
-            for key, value in data["results"][item].items():
-                add_to_dic(result, key, value)
-
-        lexicalEntries = {}
-        for item in range(len(result["lexicalEntries"][0])):
-            for key, value in result["lexicalEntries"][0][item].items():
-                add_to_dic(lexicalEntries, key, value)
-        entries = {}
-        for item in range(len(lexicalEntries["entries"])):
-            for item2 in range(len(lexicalEntries["entries"][item])):
-                for key, value in lexicalEntries["entries"][item][item2].items():
-                    add_to_dic(entries, key, value)
-        defi = {}
-        for key in entries:
-            for i in range(len(entries[key])):
-                for j in range(len(entries[key][i])):
-                    if isinstance(entries[key][i][j], str):
-                        print(entries[key][i][j])
-                    else:
-                        for k, v in entries[key][i][j].items():
-                            add_to_dic(defi, k, v)
-        subsense = {}
-        if "subsenses" in defi:
-            for item in range(len(defi["subsenses"])):
-                for item2 in range(len(defi["subsenses"][item])):
-                    for k, v in defi["subsenses"][item][item2].items():
-                        add_to_dic(subsense, k, v)
-        defi["subsenses"] = subsense
-        
-        url = 'https://pixabay.com/api/?key=18135885-6452f9fc49c5982740904652b'
-        params = {"per_page":5, "category":"computer", "safesearch":True}
-        params["q"] = word
-        response = requests.get(url, params = params)
-        image = response.json()
-        images = {}
-        for i in range(4):
-            images[i] = image["hits"][i]["webformatURL"]
-        print(images)
+        defi = get_data(word)
+        images = get_images(word)
+        misc = get_misc(word)
         defi["image"] = images
-        # print(image["hits"][0]["previewURL"])
         dic = {
-            "lexicalEntries":lexicalEntries, "defi":defi, "word":word
+            "defi":defi, "word":word, "misc":misc
         }
         return render(request, 'WordDic/searched.html', dic)
