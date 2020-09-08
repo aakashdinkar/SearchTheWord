b4fc97f7f18a61b9088327ab669be1b22e0681cd
Aakash Dinkar <aakashdinkar1307@gmail.com>
2020-09-09 01:40:25 +0530

    added some functionalities

--- Dictionary/settings.py
+++ Dictionary/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%5v#2#14ewg3a3%x(=&n!4@+29hm*xoqg%y(1b0--$ifsjasc%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['searchtheword.herokuapp.com','127.0.0.1']
 
 
 # Application definition
